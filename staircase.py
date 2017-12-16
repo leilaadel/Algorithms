31c355fa32c835a3cf18c32843860a34ad78497a
leilaadel <leilaadel96@gmail.com>
2017-12-15 16:25:58 -0800

    Uploaded staircase algo

---
+++ staircase.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+
+def staircase(n):
+    j = n-1
+    i = 1
+    while i <= n:
+        print(j*" ", end='')
+        print(i*"#")
+        i+=1
+        j-=1
+
+staircase(n)
