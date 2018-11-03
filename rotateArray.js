2bee88614666cfc171ec0e706f35cdc13ff6429d
leilaadel <leilaadel96@gmail.com>
2018-11-02 23:54:08 -0700

    added rotateArray.js

---
+++ rotateArray.js
@@ -0,0 +1,11 @@
+/**
+ *leetcode
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    for(var i = 0;i<k;i++){
+        nums.unshift(nums.pop());
+    }
+};
